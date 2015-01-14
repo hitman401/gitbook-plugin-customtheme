f8a24f0408c9ed158cbf8b2fb8df8ab3049e4820
KrishnaIndia <krishnaof1988@gmail.com>
2015-01-14 14:22:46 +0530

    JS inclusion bug fixed. Host property added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var fs = require("fs");
+var fileContentHolder = {};
+var getScriptContent = function(fileName) {
+  fileName = fileName.replace('../', '');
+  if (!fileContentHolder[fileName]) {
+    fileContentHolder[fileName] = fs.readFileSync(fileName);
+  }
+  return '<script type="text/javascript">' + fileContentHolder[fileName] + '</script>'
+};
 module.exports = {
   book: {
     assets: ".",
@@ -5,17 +14,20 @@ module.exports = {
     ],
     html: {
       "body:end": function(a) {
-        var config = this.options.pluginsConfig.customtheme || {js:[], css:[]};
+        var config = this.options.pluginsConfig.customtheme || {js:[], css:[], host: 'github'};
         var updateElements;
         updateElements = '';
         if (config.js && config.js.length > 0) {
           for (var i in config.js) {
-            updateElements += '<script type="text/javascript" src="' + config.js[i] + '"></script>';
+            updateElements += getScriptContent(config.js[i]);
           }
         }
         if (config.css && config.css.length > 0) {
+          var temp;
           for (var i in config.css) {
-            updateElements += '<link rel="stylesheet" type="text/css" href="' + config.css[i] +'">';
+            temp = config.css[i].replace('../', '')
+            temp =  (config.host === 'gitbook' ? 'content/' : '') + temp;
+            updateElements += '<link rel="stylesheet" type="text/css" href="' + '../' + temp +'">';
           }
         }
         return updateElements;

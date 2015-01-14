bae0130b9c92732cd3c0259b288a4a223475ba71
KrishnaIndia <krishnaof1988@gmail.com>
2015-01-14 17:43:31 +0530

    fs removed for javascript

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-var fs = require("fs");
-var fileContentHolder = {};
-var getScriptContent = function(fileName) {
-  fileName = fileName.replace('../', '');
-  if (!fileContentHolder[fileName]) {
-    fileContentHolder[fileName] = fs.readFileSync(fileName);
-  }
-  return '<script type="text/javascript">' + fileContentHolder[fileName] + '</script>'
-};
 module.exports = {
   book: {
     assets: ".",
     js: [
     ],
     html: {
-      "body:end": function(a) {
+      "head:end": function() {
         var config = this.options.pluginsConfig.customtheme || {js:[], css:[], host: 'github'};
         var updateElements;
         updateElements = '';
+        var getPathHierarchy = function() {
+          var temp = '';
+          var depth = this._output.split('/');
+          for (var i in depth) {
+            temp += '../';
+          }
+          return temp;
+        };
+        var resolvePath = function(fileName) {
+          var temp = fileName.replace('../', '');
+          return  getPathHierarchy() + (config.host === 'gitbook' ? 'content/' : '') + temp;
+        };
         if (config.js && config.js.length > 0) {
           for (var i in config.js) {
-            updateElements += getScriptContent(config.js[i]);
+            updateElements += '<script type="text/javascript" src="' + resolvePath(config.js[i]) + '"></script>';
           }
         }
         if (config.css && config.css.length > 0) {
           var temp;
           for (var i in config.css) {
-            temp = config.css[i].replace('../', '')
+            temp = config.css[i].replace('../', '');
             temp =  (config.host === 'gitbook' ? 'content/' : '') + temp;
-            updateElements += '<link rel="stylesheet" type="text/css" href="' + '../' + temp +'">';
+            updateElements += '<link rel="stylesheet" type="text/css" href="' + resolvePath(config.css[i]) +'">';
           }
         }
         return updateElements;
